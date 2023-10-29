35d960dcaf5a8cd9cbfdc755108da311fa250777
Sixez <sixez69@gmail.com>
2023-10-29 05:25:46 +0300

    Readme added

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Сборка проекта:
+1. ./gradlew bootJar
+2. cd build/libs/
+3. java -jar volgait-1.0.jar
+
+## Требования:  
+- java 19
+
+## URL: http://localhost:8081/swagger-ui/index.html
--- src/main/java/ru/sixez/volgait/VolgaItApplication.java
+++ src/main/java/ru/sixez/volgait/VolgaItApplication.java
@@ -3,7 +3,7 @@ package ru.sixez.volgait;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.sixez.volgait")
 public class VolgaItApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VolgaItApplication.class, args);
